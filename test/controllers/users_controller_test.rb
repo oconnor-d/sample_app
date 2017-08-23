c9fd09131192917c2ab9087ffd1d20965cb7e653
Daniel O'Connor <oconnor.d@husky.neu.edu>
2017-08-22 23:40:17 -0400

    implement admin users

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -11,6 +11,19 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "should not allow the admin attribute to be edited vi the web" do
+    log_in_as(@other_user)
+    
+    assert_not @other_user.admin?
+
+    patch user_path(@other_user), params: {
+                                    user: { password:              "password",
+                                            password_confirmation: "password",
+                                            admin: true } }
+
+    assert_not @other_user.admin?
+  end
+
   test "should redirect edit when not logged in" do
     get edit_user_path(@user)
 
