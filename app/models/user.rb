659313eb6f9024fc6c387bb6a153aaa8079e70bb
Daniel O'Connor <oconnor.d@husky.neu.edu>
2017-08-28 14:26:30 -0400

    add basic Relationship tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
 
   has_many :microposts, dependent: :destroy
   has_many :active_relationships, class_name:  'Relationship',
-                                  foreign_key: 'follower_id'
+                                  foreign_key: 'follower_id',
                                   dependent:   :destroy
 
   before_save   :downcase_email
